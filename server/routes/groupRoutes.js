0b7261fe7eb291d7d7097c546ec49f81333b0aec
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-08-18 21:09:34 +0530

    group routes created

--- server/routes/groupRoutes.js
+++ server/routes/groupRoutes.js
@@ -2,16 +2,16 @@ import express from "express";
 import {
   getNearbyGroups,
   createGroup,
-  joinGroup
+  joinGroup,
 } from "../controllers/groupController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// Find groups near user
+// Get groups near the logged-in user
 router.get("/nearby", protect, getNearbyGroups);
 
-// Create a new group
+// Create a new group (community)
 router.post("/", protect, createGroup);
 
 // Join an existing group
