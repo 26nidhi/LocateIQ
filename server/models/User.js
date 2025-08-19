899f2ad1a067ed650ab719927c307091be81167d
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-08-19 22:23:11 +0530

    user model updated

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,28 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    group: { type: mongoose.Schema.Types.ObjectId, ref: "Group" }, // Joined group
+    group: { type: mongoose.Schema.Types.ObjectId, ref: "Group" }, // optional, for joining groups
   },
   { timestamps: true }
 );
 
-export default mongoose.model("User", userSchema);
+// Encrypt password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare entered password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
