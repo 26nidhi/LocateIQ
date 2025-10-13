4ef83efa75672b79d3f25a08df2dfc056b1476cb
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-10-13 23:19:14 +0530

    undo all change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import SignInSignUp from "./pages/SignInSignUp";
-import CommunitiesPage from "./pages/Community";
+
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/auth" element={<SignInSignUp />} />
-        <Route path="/community" element={<CommunitiesPage />} />
-      </Routes>
-    </Router>
-  );
+  return <div>Hi</div>;
 }
