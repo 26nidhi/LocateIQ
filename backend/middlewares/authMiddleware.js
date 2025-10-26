45c2a019600e72b7c9cf505b5695c540d07d1724
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-10-26 23:47:33 +0530

    auth middleware created

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+// server/middlewares/authMiddleware.js
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "secretkey";
+
+export default function authMiddleware(req, res, next) {
+  const token = req.headers.authorization?.split(" ")[1]; // Bearer token
+  if (!token) return res.status(401).json({ message: "No token provided" });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = { id: decoded.id, role: decoded.role };
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+}
