97714344cab02186b9870979597e54272e7faa5b
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-11-04 00:18:22 +0530

    db.js file refactored

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,11 +6,9 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`MongoDB connection error: ${error.message}`);
-    process.exit(1);
+    console.error(`MongoDB Connection Error: ${error.message}`);
+    process.exit(1); // Exit process if DB connection fails
   }
 };
 
 export default connectDB;
-
-
