0d3d4accc74e62d2d3f8077d84f86d9c8278bfe4
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-11-04 00:39:13 +0530

    auto seed admin functionality added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,42 @@
-// server/config/db.js
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import User from "../models/User.model.js";
+import bcrypt from "bcryptjs";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    await seedAdmin(); // Seed admin after connection
   } catch (error) {
-    console.error(`MongoDB Connection Error: ${error.message}`);
-    process.exit(1); // Exit process if DB connection fails
+    console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+const seedAdmin = async () => {
+  try {
+    const adminExists = await User.findOne({ role: "admin" });
+    if (adminExists) {
+      console.log("Admin already exists");
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, 10);
+
+    const admin = await User.create({
+      name: "System Admin",
+      email: process.env.ADMIN_EMAIL,
+      password: hashedPassword,
+      role: "admin",
+    });
+
+    console.log(`Admin seeded: ${admin.email}`);
+  } catch (err) {
+    console.error("Error seeding admin:", err.message);
   }
 };
 
