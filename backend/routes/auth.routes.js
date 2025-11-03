8bd3ac4e31906e970a5981d112cacdc8316287b2
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-11-04 00:49:05 +0530

    updated auth route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,16 @@
-// server/routes/auth.routes.js
 import express from "express";
-import { register, login, me } from "../controllers/auth.controller.js";
+import {
+  register,
+  login,
+  me,
+  registerOwner,
+} from "../controllers/auth.controller.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/register-owner", registerOwner);
 router.post("/login", login);
 router.get("/me", authMiddleware, me);
 
