2326ae990b841646adf422f940a6a51147b276fc
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-11-04 00:39:46 +0530

    modified community model

--- backend/models/Community.model.js
+++ backend/models/Community.model.js
@@ -5,11 +5,22 @@ const communitySchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true, trim: true },
     description: { type: String },
-    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    location: { type: String },
     owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
     },
   },
   { timestamps: true }
